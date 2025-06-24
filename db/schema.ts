2a8c8a3048c285a719b4df8ea9da2b5ed62fdff5
Denyme24 <namanraj24@outlook.com>
2025-06-24 11:48:17 +0530

    chore: update package.json to add database scripts and dependencies

---
+++ db/schema.ts
@@ -0,0 +1,9 @@
+import {pgTable,uuid , varchar , text} from "drizzle-orm/pg-core"
+
+
+export const BlogTable = pgTable("blogs",{
+id:uuid().primaryKey().defaultRandom(),
+title:varchar({length:80}).notNull(),
+body:text().notNull(),
+orgId:text().notNull()
+})
